0588eea0f9caf8db9f8bae8357ae8cd92b019f58
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-08-27 01:21:49 +0900

    feat: 필요한 라이브러리 설치 및 적용 (#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-
 import { container } from './home.css';
 
 export default function Home() {
   return (
     <main className={container}>
-      <h2>dsad</h2>
+      <h2>홈페이지</h2>
     </main>
   );
 }
