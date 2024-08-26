4c7e98cd23057a90bf251e0486427fc0e03fcd45
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-08-27 01:54:29 +0900

    feat: globalStyle 폰트/컬러 추가 (#3)

--- src/shared/styles/fonts.css.ts
+++ src/shared/styles/fonts.css.ts
@@ -5,47 +5,38 @@ globalFontFace('Pretendard', [
   {
     src: `url('/fonts/Pretendard-Thin.woff2') format('woff2')`,
     fontWeight: '100',
-    fontStyle: 'normal',
   },
   {
     src: `url('/fonts/Pretendard-ExtraLight.woff2') format('woff2')`,
     fontWeight: '200',
-    fontStyle: 'normal',
   },
   {
     src: `url('/fonts/Pretendard-Light.woff2') format('woff2')`,
     fontWeight: '300',
-    fontStyle: 'normal',
   },
   {
     src: `url('/fonts/Pretendard-Regular.woff2') format('woff2')`,
     fontWeight: '400',
-    fontStyle: 'normal',
   },
   {
     src: `url('/fonts/Pretendard-Medium.woff2') format('woff2')`,
     fontWeight: '500',
-    fontStyle: 'normal',
   },
   {
     src: `url('/fonts/Pretendard-SemiBold.woff2') format('woff2')`,
     fontWeight: '600',
-    fontStyle: 'normal',
   },
   {
     src: `url('/fonts/Pretendard-Bold.woff2') format('woff2')`,
     fontWeight: '700',
-    fontStyle: 'normal',
   },
   {
     src: `url('/fonts/Pretendard-ExtraBold.woff2') format('woff2')`,
     fontWeight: '800',
-    fontStyle: 'normal',
   },
   {
     src: `url('/fonts/Pretendard-Black.woff2') format('woff2')`,
     fontWeight: '900',
-    fontStyle: 'normal',
   },
 ]);
 
