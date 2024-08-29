781ce5d46ac7497b11388c6ac7e1839ad2140b0d
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-08-30 04:49:06 +0900

    feat: 불필요한 파일 삭제 및 svg 사용을 위한 라이브러리 설치 (#5)

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,14 @@ const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 필요에 따라 추가 설정
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default withVanillaExtract(nextConfig);
