382550b02e0df47c83c2344fcfc3400438df4c98
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-09-01 13:36:59 +0900

    feat: rankBoard 공통 컴포넌트 생성 (#5)

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,16 @@ const nextConfig = {
 
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/**', // 모든 GitHub 사용자 프로필 이미지에 대해 허용
+      },
+    ],
+  },
 };
 
 export default withVanillaExtract(nextConfig);
