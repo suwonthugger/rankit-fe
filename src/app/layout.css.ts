b355941fb1571be416dd7114fa6ab1eb4b3c22a4
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-09-01 13:43:29 +0900

    feat: 레이아웃 main 반응형 width 설정 (#5)

--- src/app/layout.css.ts
+++ src/app/layout.css.ts
@@ -3,4 +3,13 @@ import { style } from '@vanilla-extract/css';
 export const mainStyle = style({
   display: 'flex',
   flexGrow: 1,
+  margin: '0 auto',
+  padding: '3.8rem',
+  width: '1180px',
+
+  '@media': {
+    '(max-width: 1180px)': {
+      width: '100%',
+    },
+  },
 });
