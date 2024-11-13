c682e49e95a8ac827e16121b6bb8956c774e0b70
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-11-13 21:07:35 +0900

    [Common] [Hotfix] 미들웨어 로직 추가

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from 'next/server';
 export function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
   const host = req.headers.get('host') || '';
-  const pathname = req.nextUrl.pathname;
 
   // 모바일 기기 감지
   const userAgent = req.headers.get('user-agent') || '';
@@ -13,8 +12,19 @@ export function middleware(req: NextRequest) {
     );
 
   if (isMobile) {
-    // 모바일 서브도메인으로 리다이렉션
-    url.hostname = `m.${url.hostname}`;
+    let newHostname: string;
+
+    if (host.startsWith('www.')) {
+      // 호스트네임이 'www.'로 시작하면 'www.m.'으로 변경
+      newHostname = `www.m.${host.slice(4)}`;
+    } else {
+      // 그렇지 않으면 기존 방식대로 'm.'을 앞에 추가
+      newHostname = `m.${host}`;
+    }
+
+    // 호스트네임을 변경하여 URL 업데이트
+    url.hostname = newHostname;
+
     return NextResponse.redirect(url);
   }
 
